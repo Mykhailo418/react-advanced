fc9650e9892511079f3d0036ddf21c822d82f88c
Mykhailo418 <smmichael418@gmail.com>
2018-09-04 23:24:26 +0300

    add react-motion

--- js/components/common/Trash.js
+++ js/components/common/Trash.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { DropTarget } from 'react-dnd';
 import { type as eventRowType } from '../events/TableRow';
+import { Motion, spring } from 'react-motion';
 
 let componentStyle = {
   fontSize: '3em'
@@ -12,9 +13,16 @@ class Trash extends Component {
      const color = canDrop ? (hovered ? 'red' : 'green') : 'black';
      console.log(eventRowType);
      return (
-        connectDropTarget(
-            <i style={{color ,...componentStyle}} className="fa fa-trash-o"></i>
-        )
+       <Motion
+         style={{ opacity: spring(1, { stiffness: 40, damping: 40 }) }}
+         defaultStyle={{ opacity: 0 }}
+       >
+           {(interpolatedStyle) =>
+              connectDropTarget(
+                <i style={{color ,...componentStyle, ...interpolatedStyle}} className="fa fa-trash-o"></i>
+            )
+          }
+        </Motion>
      );
    }
 }
--- js/components/events/SelectedEvents.js
+++ js/components/events/SelectedEvents.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { selectedEventsSelector } from '../../widgets/events';
 import EventCard from './EventCard';
+import { TransitionMotion, spring } from 'react-motion';
+
+const motionSettings = { stiffness: 50, damping: 30 };
 
 class SelectedEvents extends Component{
 	static proptypes = {
@@ -10,22 +13,52 @@ class SelectedEvents extends Component{
 	}
 
 	render(){
+		if(!this.props.selectedEvents){ return null; }
 		return(
-			<ul>
-				{this.outputEventsList()}
-			</ul>
+			<TransitionMotion
+        styles={this.getStyles()}
+        willEnter={this.willEnter}
+        willLeave={this.willLeave}
+      >
+			 {(interpolated) => (
+					<ul>
+						{this.outputEventsList(interpolated)}
+					</ul>
+				)}
+			</TransitionMotion>
 		);
 	}
 
-	outputEventsList = () => {
-		const {selectedEvents} = this.props;
-		//console.log(selectedEvents, selectedEvents.length);
-		if(!selectedEvents.size) return <li>There are no selected events</li>
-		//console.log(selectedEvents.toJS());
-		return selectedEvents.valueSeq().toArray().map((event) => {
-			return <EventCard key={event.uid} event={event} />;
+	outputEventsList = (interpolated) => {
+		if(!interpolated || !interpolated.length) return <li>There are no selected events</li>
+		//console.log('interpolated = ',interpolated);
+		return interpolated.map((data, index) => {
+			return (
+				<div key={data.key} style={{...data.style }}>
+					<EventCard event={data.data} />
+				</div>
+			);
 		});
 	}
+
+	willEnter = () => ({
+    opacity: 0
+  })
+
+	willLeave = () => ({
+    opacity: spring(0, motionSettings)
+  })
+
+	getStyles(){
+		let arr = this.props.selectedEvents.valueSeq().toArray();
+    return arr.map((event) => ({
+      key: event.uid,
+      style: {
+        opacity: spring(1, motionSettings)
+      },
+      data: event
+    }))
+  }
 }
 
 function mapToProps(state){
